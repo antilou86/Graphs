de98a504a61779ed5069d0a9343352fca232fdb6
luis guzman <antilou86@gmail.com>
2020-01-21 17:02:47 -0500

    partially completed psuedo-code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,24 @@
 
 def earliest_ancestor(ancestors, starting_node):
+    #takes in a list of tuples representing a graph
+    #also accepts a starting node
+    #must find the furthest ancestor
+        #if tied, lowest ID wins
+        #if no parents, return -1
+
+    #make a copy of the ancestors
+    #also make a set of nodes
+
+    #loop through each item in the copy of ancestors
+    #maybe use a while loop?
+        #check the first node's children
+        #pop ancestor off and check to see where it points?
+        #if ancestor doesnt exist in tracking set, add it.
+            #key is the node id. will need a distance and maybe a parent node reference?
+
+    #return -1 if the node doesnt exist in the node set.
+
+    # 
     pass
+
+
